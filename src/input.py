3e4e228eb8fcf6cbd7e3bc197297e3d7a66194d8
Silke Maes <silke.maes@kuleuven.be>
2023-05-29 15:10:08 +0200

    Changing & adding functions for easy development.

--- src/input.py
+++ src/input.py
@@ -1,7 +1,40 @@
 from astropy import constants   as cst
+from astropy import units as units
 import numpy as np
 
 
+## Physical constants
+kB = cst.k_B.cgs.value          ## Boltzmann constant [erg/K]
+mH = cst.u.to('g').value        ## mass H atom = atomic mass unit [g]
+Msun = cst.M_sun.cgs.value      ## gram
+yr = units.year.to('s')         ## year in seconds
+Msunyr = Msun/yr                ## units of mass-loss rate in gram/s
+cms  = 1e5                      ## units of velocity in cm/s
+g_to_kg = units.gram.to('kg')
+cm_to_m = units.cm.to('m')
+
+mu = 2.0 + 4.0*0.17             ## mu (average mass per H2 molecule), taking into account the abundance of He
+
+
+def density(Mdot,v, r):
+    '''
+    Input 
+        - mass-loss rate (Mdot) in units of Msol/yr
+        - outflow velocity (v) in units of km/s
+        - radius (r): location of the outflow, inputs of cm
+    Output
+        - density in units of g/cm^3
+    '''
+    # r    = 1e18 #* unt.cgs.cm                       # cm
+    Mdot = Mdot * Msunyr                            # gram/s
+    v    = v    * cms                               # cm/s
+
+
+    dens = Mdot / (4*np.pi * v * r**2 * mu * mH)       # g/cm^3
+
+    # dens = dens * g_to_kg * cm_to_m**(-3)           # kg/m^3
+
+    return dens
 
 ## input values physics
 def setinput():
@@ -17,7 +50,8 @@ def setinput():
     '''
 
     ## input physics
-    ρ = 1.e+6
+    ρ = density(1.e-7, 10., 1.e15)
+    # ρ = 1.e+6
     T = 2500.
     δ = 1.      
     Av = 0.5   
@@ -27,6 +61,15 @@ def setinput():
 
     r = 1.        ## setting inwards geometrical dilution
     ΔAv = 1.      ## inwards dust extinction 
+
+    print('Input:')
+    print('------')
+    print('ρ  =','{:.2E}'.format(ρ))
+    print('T  =',T)
+    print('δ  =',δ)
+    print('Av =',Av)
+    print('')
+    print('Chem type =', chemtype)
 
     return ρ, T, δ, Av, chemtype
 
