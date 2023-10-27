dcb6998897313e9dfdf73ef5510fe481ddb03dec
Silke Maes <silke.maes@kuleuven.be>
2023-10-27 11:35:57 +0200

    First benchmark test --> dt not long enough, runnen new 0D models

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -7,6 +7,7 @@ import datetime         as dt
 from astropy import constants   as cst
 from astropy import units       as units
 from astropy.constants          import M_sun
+import matplotlib.pyplot        as plt
 
 import json
 
@@ -47,11 +48,11 @@ rate = 16
 
 ## location to save benchmark
 out = '/STER/silkem/ChemTorch/out/'
-dirname = 'bm_1'
+dirname = 'bm_2'
 
 ## 1D chem model
-outloc = '/lhome/silkem/CHEM/Output_nov2022/'
-outdir = '20210521_gridC_Mdot1e-6_v15_T_eps/'
+outloc = '/STER/silkem/CSEchem/'
+outdir = '20210521_gridC_Mdot1e-6_v15_T_eps-'
 mod = 'model_2022-12-26h13-01-25'
 
 makeOutputDir(out+dirname+'/')
@@ -64,16 +65,19 @@ T_star = 2500
 solvertype = 'ivp'
 
 ## loading the physical input from the 1D model
-arr = np.loadtxt(outloc+outdir+'models/'+mod+'/csphyspar_smooth.out', skiprows=4, usecols=(0,1,2,3,4,11))
+arr = np.loadtxt(outloc+outdir+mod+'/csphyspar_smooth.out', skiprows=4, usecols=(0,1,2,3,4,11))
 radius, dens, temp, Av, δ, delta_AUV = arr[:,0], arr[:,1], arr[:,2], arr[:,3], arr[:,4], arr[:,5]
 time = radius/(v* cms)
-time = 10**(np.log10(time)-np.log10(time)[0])
+# time = 10**(np.log10(time)-np.log10(time)[0])
 chemtype = 'C' 
+
+# t = time
+
+
 
 dt = np.zeros(len(time))
 for i in range(1,len(time)):
     dt[i] = (time[i]-time[i-1])
-
 
 
 ## metadata
@@ -96,5 +100,6 @@ name = ''
 
 for i in range(len(dens)):
     input = [dens[i], temp[i], δ[i], Av[i]]
-    n, name = solve(input, dt[i], rate, n, nshield_i, nconsv_tot, name, dirname=dirname, solvertype = solvertype)
-		
+    n, name = solve(input, dt[i], rate, n, nshield_i, nconsv_tot, name, dirname=dirname, solvertype = solvertype) # type: ignore
+
+
