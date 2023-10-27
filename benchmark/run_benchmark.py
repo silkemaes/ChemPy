d9bad725b0a8a6052669ca77fb8dcd0c0f0d4ea0
Silke Maes <silke.maes@kuleuven.be>
2023-10-27 09:25:50 +0200

    Checking the benchmark

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -33,7 +33,7 @@ mu = 2.0 + 4.0*0.17             ## mu (average mass per H2 molecule), taking int
 
 sys.path.append('/STER/silkem/ChemTorch/')
 
-from src.solve_n_save       import solve_dg
+from src.solve_n_save       import solve
 from src.input              import density
 import src.rates            as rates
 
@@ -46,34 +46,55 @@ def makeOutputDir(path):
 rate = 16
 
 ## location to save benchmark
-out = '/STER/silkem/ChemTorch/benchmark/'
-dirname = 'bm'+str(dt.datetime.now())
+out = '/STER/silkem/ChemTorch/out/'
+dirname = 'bm_1'
 
 ## 1D chem model
 outloc = '/lhome/silkem/CHEM/Output_nov2022/'
 outdir = '20210521_gridC_Mdot1e-6_v15_T_eps/'
 mod = 'model_2022-12-26h13-01-25'
 
-
+makeOutputDir(out+dirname+'/')
 
 ## input
 Mdot = 1.e-6
 v = 15.
 eps = 0.6
 T_star = 2500
+solvertype = 'ivp'
 
-
+## loading the physical input from the 1D model
 arr = np.loadtxt(outloc+outdir+'models/'+mod+'/csphyspar_smooth.out', skiprows=4, usecols=(0,1,2,3,4,11))
-radius, n_H, temp, AUV, RAD, delta_AUV = arr[:,0], arr[:,1], arr[:,2], arr[:,3], arr[:,4], arr[:,5]
+radius, dens, temp, Av, δ, delta_AUV = arr[:,0], arr[:,1], arr[:,2], arr[:,3], arr[:,4], arr[:,5]
 time = radius/(v* cms)
+time = 10**(np.log10(time)-np.log10(time)[0])
 chemtype = 'C' 
 
+dt = np.zeros(len(time))
+for i in range(1,len(time)):
+    dt[i] = (time[i]-time[i-1])
 
-metadata = {'1Dmodel' : outloc+outdir+mod,
-        'dens' : ,
+
+
+## metadata
+metadata = {'1Dmodel'   : outloc+outdir+mod,
+            'Mdot'      : Mdot,
+            'v'         : v,
+            'eps'       : eps,
+            'T_star'    : T_star,
+            'solvertype': solvertype
         }
-
 
 json_object = json.dumps(metadata, indent=4)
 with open(out+dirname+"/meta.json", "w") as outfile:
     outfile.write(json_object)
+
+
+## set initial conditions
+n, nconsv_tot, specs, nshield_i = rates.initialise_abs(chemtype, rate)    
+name = '' 
+
+for i in range(len(dens)):
+    input = [dens[i], temp[i], δ[i], Av[i]]
+    n, name = solve(input, dt[i], rate, n, nshield_i, nconsv_tot, name, dirname=dirname, solvertype = solvertype)
+		
